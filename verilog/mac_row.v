2a91dd5bc8e225cb307836c050e2ea1a86e31096
raj <suraj.s2306@gmail.com>
2023-11-28 23:29:30 -0800

    added verilog basic files

---
+++ verilog/mac_row.v
@@ -0,0 +1,36 @@
+module mac_row (clk, out_s, in_w, in_n, valid, inst_w, reset);
+
+  parameter bw = 4;
+  parameter instr_bw = 2;
+  parameter psum_bw = 16;
+  parameter col = 8;
+
+  input  clk, reset;
+  output [psum_bw*col-1:0] out_s;
+  output [col-1:0] valid;
+  input  [bw-1:0] in_w; // inst[1]:execute, inst[0]: kernel loading
+  input  [instr_bw:0] inst_w;
+  input  [psum_bw*col-1:0] in_n;
+
+  wire  [(col+1)*bw-1:0] temp;
+  reg [col-1:0] validTemp = 8'b0;
+  wire  [(col+1)*instr_bw-1:0] instr_temp;
+
+  assign temp[bw-1:0]   = in_w;
+  assign instr_temp[instr_bw-1:0]   = inst_w;
+
+  genvar i;
+  for (i=1; i < col+1 ; i=i+1) begin : col_num
+      mac_tile #(.bw(bw), .psum_bw(psum_bw)) mac_tile_instance (
+         .clk(clk),
+         .reset(reset),
+	 .in_w(temp[bw*i-1:bw*(i-1)]),
+	 .out_e(temp[bw*(i+1)-1:bw*i]),
+	 .inst_w(instr_temp[instr_bw*i-1:instr_bw*(i-1)]),
+	 .inst_e(instr_temp[instr_bw*(i+1)-1:instr_bw*i]),
+	 .in_n(in_n[psum_bw*i-1:psum_bw*(i-1)]),
+	 .out_s(out_s[psum_bw*i-1:psum_bw*(i-1)]));
+   assign  valid[i-1] = instr_temp[(i+1)*instr_bw-1];
+  end
+
+endmodule
---
+++ verilog/mac_tile.v
@@ -0,0 +1,62 @@
+
+
+module mac_tile (
+    clk,
+    out_s,
+    in_w,
+    out_e,
+    in_n,
+    inst_w,
+    inst_e,
+    reset
+);
+
+  parameter bw = 4;
+  parameter psum_bw = 16;
+
+  output [psum_bw-1:0] out_s;
+  input [bw-1:0] in_w;  // inst[1]:execute, inst[0]: kernel loading
+  output [bw-1:0] out_e;
+  input [1:0] inst_w;
+  output [1:0] inst_e;
+  input [psum_bw-1:0] in_n;
+  input clk;
+  input reset;
+
+  reg [bw-1:0] a_q;
+  reg [bw-1:0] b_q;
+  reg [psum_bw-1:0] c_q;
+  reg [1:0] inst_q;
+  reg load_ready_q;
+  wire [psum_bw-1:0] mac_out;
+
+  mac #(
+      .bw(bw),
+      .psum_bw(psum_bw)
+  ) mac_instance (
+      .a(a_q),  //activation
+      .b(b_q),  //weight
+      .c(c_q),  //psum
+      .out(mac_out)
+  );
+
+  assign inst_e = inst_q;
+  assign out_e  = a_q;
+  assign out_s  = mac_out;
+  always @(posedge clk) begin
+    if (reset == 1) begin
+      inst_q = 2'b0;
+      load_ready_q = 1'b1;
+
+    end else begin
+      c_q <= in_n;
+      inst_q[1] <= inst_w[1];
+      if (inst_w[0] | inst_w[1]) a_q <= in_w;
+      if (inst_w[0] == 1 && load_ready_q == 1) begin
+        b_q <= in_w;
+        load_ready_q <= 1'b0;
+      end
+      if (load_ready_q == 0) inst_q[0] <= inst_w[0];
+    end
+  end
+endmodule
