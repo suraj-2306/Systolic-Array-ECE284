905d897b63cf9035d54669392b4e351a5fbefe9c
raj <suraj.s2306@gmail.com>
2023-12-01 23:32:35 -0800

    formatted code

--- verilog/corelet.v
+++ verilog/corelet.v
@@ -1,10 +1,12 @@
 //We need add the 
 //On the other hand, a corelet.v includes all the other blocks (e.g., L0, 2d PE array, ofifo) other than SRAMs.
 // module l0 (clk, in, out, rd, wr, o_full, reset, o_ready);
-module corelet (input clk);
+module corelet (
+    input clk
+);
 
   parameter col = 8;
-  parameter row= 8;
+  parameter row = 8;
   parameter bw = 4;
   parameter psum_bw = 16;
   parameter total_cycle = 64;
@@ -21,49 +23,59 @@ module corelet (input clk);
 
 
   //l0 operations. Input can be the instructions or data
-  l0 #( .bw(bw))
-  l0_instance (
-    .clk(clk),
-    .in(w_vector_bin),
-    .out(l0out),
-    .rd(l0rd),
-    .wr(l0wr),
-    .o_full(l0full),
-    .reset(l0reset),
-    .o_ready(l0ready)
+  l0 #(
+      .bw(bw)
+  ) l0_instance (
+      .clk(clk),
+      .in(w_vector_bin),
+      .out(l0out),
+      .rd(l0rd),
+      .wr(l0wr),
+      .o_full(l0full),
+      .reset(l0reset),
+      .o_ready(l0ready)
   );
 
 
-reg [col*bw-1:0] ofwr;
-reg ofrd;
-reg    ofreset;
-wire [col*psum_bw-1:0] ofin;
-wire   [col*psum_bw-1:0] ofout;
-wire   ofo_full;
-wire   ofo_ready;
-wire   ofo_valid;
+  reg [col*bw-1:0] ofwr;
+  reg ofrd;
+  reg    ofreset;
+  wire [col*psum_bw-1:0] ofin;
+  wire   [col*psum_bw-1:0] ofout;
+  wire   ofo_full;
+  wire   ofo_ready;
+  wire   ofo_valid;
 
-  ofifo #( .bw(bw))
-  ofifo_instance (
-    .clk(clk),
-    .in(ofin),
-    .out(ofout),
-    .rd(ofrd),
-    .wr(ofwr),
-    .o_full(ofo_full),
-    .reset(ofreset),
-    .o_ready(ofo_ready),
-    .o_valid(ofo_valid)
+  ofifo #(
+      .bw(bw)
+  ) ofifo_instance (
+      .clk(clk),
+      .in(ofin),
+      .out(ofout),
+      .rd(ofrd),
+      .wr(ofwr),
+      .o_full(ofo_full),
+      .reset(ofreset),
+      .o_ready(ofo_ready),
+      .o_valid(ofo_valid)
   );
 
 
-  reg  macreset;
+  reg macreset;
   wire [psum_bw*col-1:0] macout_s;
-  reg [row*bw-1:0] macin_w; // inst[1]:execute, inst[0]: kernel loading
+  reg [row*bw-1:0] macin_w;  // inst[1]:execute, inst[0]: kernel loading
   reg [1:0] macinst_w;
   reg [psum_bw*col-1:0] macin_n;
   wire [col-1:0] macvalid;
 
-mac_array mac_array_instance(.clk(clk), .reset(macreset), .out_s(macout_s), .in_w(macin_w), .in_n(macin_n), .inst_w(macinst_w), .valid(macvalid));
+  mac_array mac_array_instance (
+      .clk(clk),
+      .reset(macreset),
+      .out_s(macout_s),
+      .in_w(macin_w),
+      .in_n(macin_n),
+      .inst_w(macinst_w),
+      .valid(macvalid)
+  );
 
 endmodule
