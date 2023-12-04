abc0f6177aa9e8d7123ea97f0d4b55b4fc7d68b4
Yash <yashxd01@gmail.com>
2023-12-04 13:26:56 -0800

    Fix sfu1.v operation

--- verilog/sfu_tb.v
+++ verilog/sfu_tb.v
@@ -10,9 +10,10 @@ module sfu_tb;
   reg valid;
   reg reset;
 
+  integer psum_in_val = 0;
   integer i = 0;
   integer j = 0;
-  integer validate[input_ch];
+  integer validate[input_ch-1:0];
   reg send_out=0;
   reg clk=0;
 
@@ -43,18 +44,37 @@ module sfu_tb;
     end
 
     for (i=0; i<input_ch*num_iters; i=i+1) begin
-      #2 clk = 1'b0;   psum_in = i;
-      #2 clk = 1'b1;
-      validate[j] = validate[j] + i;
-      j = j+1;
+      if (i > 60)
+        psum_in_val = -100;
+      else
+        psum_in_val = i;
+
+      psum_in = psum_in_val;
+      validate[j] = validate[j] + psum_in_val;
+      #4 j = j+1;
       if (j == input_ch) begin
         j = 0;
       end
     end
-    valid=0;
+    send_out = 1'b1;
+    valid = 1'b0;
 
-    #2 send_out = 1'b1;
+    // Perform Relu operation on validate
+    // NOTE: Disable for checking negative values.
+    for (i=0; i<input_ch; i=i+1) begin
+      if (validate[i] < 0)
+        validate[i] = 0;
+    end
 
+    #4
+    for (i=0; i<input_ch; i=i+1) begin
+      if (validate[i] == psums_out[psum_bw*i +: psum_bw]) begin
+        $display("Partial Sum output validated for reg %d: psum_out = %d", i, psums_out[psum_bw*i +: psum_bw]);
+      end
+      else begin
+        $display("Partial Sum output NOT MATCHED for reg %d: psum_out = %d, validate = %d", i, psums_out[psum_bw*i +: psum_bw], validate[i]);
+      end
+    end
     #10 $finish;
   end
 
