f15c54987cdee01552d0b0e12a9ee3b0a466370a
raj <suraj.s2306@gmail.com>
2023-12-15 20:39:04 -0800

    added generate endgenerate block

--- src/mac_array.v
+++ src/mac_array.v
@@ -27,16 +27,18 @@ module mac_array (clk, reset, out_s, in_w, in_n, inst_w, valid, cascade);
   assign temp[psum_bw*col*1-1:psum_bw*col*0] = 0;
   assign valid = valid_temp[row*col-1:row*col-8];
 
-  for (i=1; i < row+1 ; i=i+1) begin : row_num
-      mac_row #(.bw(bw), .psum_bw(psum_bw)) mac_row_instance (
-         .clk(clk),
-         .reset(reset),
-	 .in_w(in_w[bw*i-1:bw*(i-1)]),
-	 .inst_w(inst_w_temp[2*i-1:2*(i-1)]),
-	 .in_n(temp[psum_bw*col*i-1:psum_bw*col*(i-1)]),
-         .valid(valid_temp[col*i-1:col*(i-1)]),
-	 .out_s(temp[psum_bw*col*(i+1)-1:psum_bw*col*(i)]));
-  end
+  generate
+    for (i=1; i < row+1 ; i=i+1) begin : row_num
+        mac_row #(.bw(bw), .psum_bw(psum_bw)) mac_row_instance (
+           .clk(clk),
+           .reset(reset),
+     .in_w(in_w[bw*i-1:bw*(i-1)]),
+     .inst_w(inst_w_temp[2*i-1:2*(i-1)]),
+     .in_n(temp[psum_bw*col*i-1:psum_bw*col*(i-1)]),
+           .valid(valid_temp[col*i-1:col*(i-1)]),
+     .out_s(temp[psum_bw*col*(i+1)-1:psum_bw*col*(i)]));
+    end
+  endgenerate
 
   always @ (posedge clk) begin
 
